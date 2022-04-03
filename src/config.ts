a58e6c74599570d1258cb4c4e12c190960d1d3e4
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-04-03 23:43:29 +0900

    Add animation

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,26 @@ export const CONFIG = {
   fontSize: {
     sp: '28vw',
     pc: '300px'
+  },
+  cube: {
+    size: {
+      width: 100,
+      height: 100,
+      depth: 100
+    },
+    color: {
+      re: 0xf44336,
+      bl: 0x03a9f4,
+      gr: 0x4caf50,
+      ye: 0xffeb3b,
+      or: 0xffc107,
+      wh: 0xffffff
+    }
+  },
+  light: {
+    color: 0xffffff
+  },
+  backGround: {
+    color: 0xf5f5f5
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,72 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { CONFIG } from './config';
 
-window.addEventListener('load', () => {
+const positionKeyframeTrackJSON = {
+  name: '.position',
+  type: 'vector',
+  times: [0, 1, 2],
+  values: [0, 0, 0, 2, 1, 15, 0, 0, 0]
+};
+
+const rotationKeyframeTrackJSON = {
+  name: '.rotation[y]',
+  type: 'number',
+  times: [0, 2],
+  values: [0, 2 * Math.PI],
+  interpolation: THREE.InterpolateSmooth
+};
+
+const clipJSON = {
+  duration: 2,
+  tracks: [positionKeyframeTrackJSON, rotationKeyframeTrackJSON]
+};
+const clip = THREE.AnimationClip.parse(clipJSON);
+
+const materials: THREE.MeshBasicMaterial[] = [
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.re }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.bl }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.gr }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.ye }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.or }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.wh }),
+  new THREE.MeshBasicMaterial({ color: CONFIG.cube.color.re })
+];
+
+function createCubeGroup(): THREE.Group {
+  const { width, height, depth } = CONFIG.cube.size;
+  const group: THREE.Group = new THREE.Group();
+  [-1, 0, 1].forEach((x) => {
+    [-1, 0, 1].forEach((y) => {
+      [-1, 0, 1].forEach((z) => {
+        const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(width - 20, height - 20, depth - 20);
+        const cube: THREE.Mesh = new THREE.Mesh(geometry, materials);
+        cube.position.set(x * width, y * height, z * depth);
+        group.add(cube);
+      });
+    });
+  });
+  return group;
+}
+
+window.onload = function () {
   const scene: THREE.Scene = new THREE.Scene();
 
   // Camera
   const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(45, 640 / 480, 1, 10000);
   camera.position.set(0, 0, 1000);
 
-  const pointLight = new THREE.PointLight(0xffffff);
+  const pointLight = new THREE.PointLight(CONFIG.light.color);
   camera.add(pointLight);
 
   scene.add(camera);
 
   // Cube
-  const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(100, 100, 100);
-  const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-  const cube: THREE.Mesh = new THREE.Mesh(geometry, material);
-
+  const cube: THREE.Group = createCubeGroup();
   scene.add(cube);
 
   // Light
-  const light: THREE.DirectionalLight = new THREE.DirectionalLight(0xffffff);
+  const light: THREE.DirectionalLight = new THREE.DirectionalLight(CONFIG.light.color);
   light.position.set(1, 1, 1);
 
   scene.add(light);
@@ -29,11 +74,10 @@ window.addEventListener('load', () => {
   // Renderer
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0xffffff);
+  renderer.setClearColor(CONFIG.backGround.color);
   document.body.appendChild(renderer.domElement);
 
   const controls: OrbitControls = new OrbitControls(camera, renderer.domElement);
-  controls.update();
 
   // Helpers
   const axes = new THREE.AxesHelper(1000);
@@ -42,10 +86,25 @@ window.addEventListener('load', () => {
   // const grid = new THREE.GridHelper(window.innerWidth, window.innerHeight);
   // scene.add(grid);
 
+  // Animaation
+  var mixer = new THREE.AnimationMixer(cube);
+  var action = mixer.clipAction(clip);
+  action.play();
+
+  window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  };
+
   const tick = (): void => {
-    renderer.render(scene, camera);
     requestAnimationFrame(tick);
+
+    mixer.update(0.01);
+    controls.update();
+    renderer.render(scene, camera);
   };
 
   tick();
-});
+};
