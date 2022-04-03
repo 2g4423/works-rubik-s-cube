bec4b56e1b7e4bf2b83ac03bc5445aff9de707b0
2g4423 <yoshifumi.tsuji.biz@gmail.com>
2022-04-03 20:33:28 +0900

    Update camera and light

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import * as THREE from 'three';
-import { getScreenSize } from './utils';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const size = getScreenSize();
-
-  const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer();
-  renderer.setSize(size.width, size.height);
-  document.body.appendChild(renderer.domElement);
-
+window.addEventListener('load', () => {
   const scene: THREE.Scene = new THREE.Scene();
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xffffff);
+  document.body.appendChild(renderer.domElement);
 
   const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(45, 640 / 480, 1, 10000);
   camera.position.set(0, 0, 1000);
+
+  const pointLight = new THREE.PointLight(0xffffff);
+  camera.add(pointLight);
+
+  const controls: OrbitControls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+
+  scene.add(camera);
 
   const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(100, 100, 100);
   const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
@@ -21,13 +28,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const light: THREE.DirectionalLight = new THREE.DirectionalLight(0xffffff);
   light.position.set(1, 1, 1);
+
   scene.add(light);
 
   const tick = (): void => {
     requestAnimationFrame(tick);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    // cube.rotation.z += 0.01;
 
     renderer.render(scene, camera);
   };
